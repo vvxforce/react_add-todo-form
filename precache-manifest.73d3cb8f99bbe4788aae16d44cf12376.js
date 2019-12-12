6aa50e151bcddeefbc4d8744451042d1ddfd9464
Vvxforce <vvxforce@gmail.com>
2019-12-12 13:11:19 +0200

    Updates

---
+++ precache-manifest.73d3cb8f99bbe4788aae16d44cf12376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fbb1340069fd4e12a3592563a4a51c",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "9159c57e7440630e34f6",
+    "url": "/react_add-todo-form/static/css/main.e583a1a5.chunk.css"
+  },
+  {
+    "revision": "710e1266e890094367a4",
+    "url": "/react_add-todo-form/static/js/2.36d60507.chunk.js"
+  },
+  {
+    "revision": "9159c57e7440630e34f6",
+    "url": "/react_add-todo-form/static/js/main.5ecc1fb4.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
